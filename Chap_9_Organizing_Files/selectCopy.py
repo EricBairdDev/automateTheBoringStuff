b20eba96d4319b10e7055163e9ebd63ac8bf3ed1
homer2020 <ericmbaird@gmail.com>
2022-12-03 14:51:25 -0500

    init

---
+++ Chap_9_Organizing_Files/selectCopy.py
@@ -0,0 +1,12 @@
+#! python3
+# selectCopy.py - prompts for a specific file type and copies files to another folder
+
+# TODO: walk through folder tree
+
+# TODO: prompt user what file type
+
+# TODO: search for file type
+
+# TODO: copy file type
+
+# TODO: paste in new directory
