d6803cc3cce22c905d29374f7cad967702acecde
homer2020 <ericmbaird@gmail.com>
2022-12-03 22:24:40 -0500

    WIP

--- Chap_9_Organizing_Files/gapFill.py
+++ Chap_9_Organizing_Files/gapFill.py
@@ -4,18 +4,25 @@
 
 import os, shutil
 
-folderName = 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\Chap_9_Organizing_Files\\gap_fill'
+fileType = []
+folderName = 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\Chap_9_Organizing_Files\\gap_fill\\'
+print('Enter new file prefix')
+prefix = str(input())
 
 #check file in folder
 folder = os.listdir(folderName)
 
 for files in range(len(folder)):
     #temp print
-    print(files)
     print(folder[files])
+    fileStr = str(folder[files])
 
     # check names of files
-
+    #shutil.move(folderName + [files])
+    print(folderName + folder[files])
+    print(folderName + prefix + str(files))
     # copy and rename file in correct order
 #delete old file
 
+if folder[0][files] == '.':
+    fileType.append(folder[0][fileType])
