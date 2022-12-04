c0666d6b2ac1188d89a22e3a23046fedfa7740fe
homer2020 <ericmbaird@gmail.com>
2022-12-03 19:57:42 -0500

    added TODOs

--- Chap_9_Organizing_Files/selectCopy.py
+++ Chap_9_Organizing_Files/selectCopy.py
@@ -1,33 +1,36 @@
 #! python3
-# selectCopy.py - prompts for a specific file type and copies files to another folder
+# selectCopy.py - prompts for a specific file type and copies files to
+# C:\\Users\\ericm\\PycharmProjects\\ATBS\\backupfiles
 
 import os, shutil
 
-# TODO: walk through folder tree
-
-print(r'Input folder tree to copy. (Include "\\")')
-#folder = str(input())
-folder = 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\Chap_9_Organizing_Files'
-
-
-# TODO: prompt user what file type
-
-print(r'What file type would you like to copy? Include "."')
-fileType = str(input())
-
-
-# TODO: search for file type
-
-for folderName, subfolders, filenames in os.walk(folder):
-    for filename in filenames:
-        if filename.endswith(fileType):
-            #test print
-            print(folderName + '\\' + filename)
-            # copy files
-            try:
-                shutil.copy(folderName + '\\' + filename, '/backupfiles')
-            except shutil.SameFileError:
-                print('Already backed')
+# search for file type
+def searchFolder(folder):
+    for folderName, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(fileType):
+                #test print
+                print(folderName + '\\' + filename + ' --copied')
+                # copy files
+                try:
+                    shutil.copy(folderName + '\\' + filename, 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\backupfiles\\' + 'backup_' + filename)
+                except shutil.SameFileError:
+                    continue
 
 
+    # walk through folder tree
+    print(r'Input folder tree to copy. (Include "\\")')
+    # TODO: Allow for input
+    #folder = str(input()) #uncomment to allow input
+    # TODO: Check if directory exists or not
+    folder = 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\Chap_9_Organizing_Files'
+
+    # prompt user what file type
+    print(r'What file type would you like to copy? Include "."')
+    fileType = str(input())
+    break
+
+# TODO: Add ability to copy another folder/filetype, create function
+
+searchFolder(folder)
 
