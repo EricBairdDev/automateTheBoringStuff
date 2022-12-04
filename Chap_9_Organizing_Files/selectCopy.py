e36dd8e405637eb0bb639595ca3d5ee7e7fdecf3
homer2020 <ericmbaird@gmail.com>
2022-12-03 19:27:55 -0500

    add copy

--- Chap_9_Organizing_Files/selectCopy.py
+++ Chap_9_Organizing_Files/selectCopy.py
@@ -1,12 +1,33 @@
 #! python3
 # selectCopy.py - prompts for a specific file type and copies files to another folder
 
+import os, shutil
+
 # TODO: walk through folder tree
+
+print(r'Input folder tree to copy. (Include "\\")')
+#folder = str(input())
+folder = 'C:\\Users\\ericm\\PycharmProjects\\ATBS\\Chap_9_Organizing_Files'
+
 
 # TODO: prompt user what file type
 
+print(r'What file type would you like to copy? Include "."')
+fileType = str(input())
+
+
 # TODO: search for file type
 
-# TODO: copy file type
+for folderName, subfolders, filenames in os.walk(folder):
+    for filename in filenames:
+        if filename.endswith(fileType):
+            #test print
+            print(folderName + '\\' + filename)
+            # copy files
+            try:
+                shutil.copy(folderName + '\\' + filename, '/backupfiles')
+            except shutil.SameFileError:
+                print('Already backed')
 
-# TODO: paste in new directory
+
+
