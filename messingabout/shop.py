1a6de846fd461599602b0d91d89faee0d3971579
homer2020 <ericmbaird@gmail.com>
2022-12-04 22:37:34 -0500

    init

--- messingabout/shop.py
+++ messingabout/shop.py
@@ -2,16 +2,27 @@
 # shop.py - Can purchase items from a shop keep inventory.
 
 import os, shelve
-from items import npcInv, itemCost      #get items
-
-
-
-
+from items import npcInv, itemCost
 
 # check to see what items the shop has and how many and how much
-for item, amount in npcInv.items():
-    print(item + ' x ' + str(amount))
+def checkItems():
+    for item, amount in npcInv.items():
+        print(item + ' x ' + str(amount))
+
 # ask to buy something
+def shopKeep():
+    while True:
+        print('Would you like to buy something? (Y/N)')
+        yesNo = input().upper()
+        if yesNo == 'Y':
+            print('These are the items we have in stock:')
+            checkItems()
+        elif yesNo == 'N':\
+            exit()
+        else:
+            continue
+
+shopKeep()
 # accept player prompt
 # deduct item and cost
 # show new inventory
