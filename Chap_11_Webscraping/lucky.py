673730b011b83748894acbce7165e1ffb25c5788
homer2020 <ericmbaird@gmail.com>
2022-12-08 20:25:04 -0500

    init

--- Chap_11_Webscraping/lucky.py
+++ Chap_11_Webscraping/lucky.py
@@ -4,7 +4,7 @@
 import requests, sys, webbrowser, bs4
 
 print('Googling...') #displayu text while downloading the google page
-res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://www.google.com/search?q=linguistics')
 res.raise_for_status()
 
 # TODO: Retrieve top search result links.
@@ -12,3 +12,6 @@ soup = bs4.BeautifulSoup(res.text)
 
 #  TODO: Open a browser tab for each result
 linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
