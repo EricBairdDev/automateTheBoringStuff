282390fda7339de146526ec499553a1ffc228809
homer2020 <ericmbaird@gmail.com>
2022-12-10 00:09:29 -0500

    init

--- Chap_12_Excel/readCensusExcel.py
+++ Chap_12_Excel/readCensusExcel.py
@@ -1,3 +1,10 @@
 #1 python3
 # readCensusExcel.py - Tabulates population and number of census tracts for each county
 
+import openpyxl, pprint
+print('Opening workbook...')
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb['Population by Census Tract']
+countyData = {}
+
+#TODO: Fill in countyData with each county's pop and tracts
