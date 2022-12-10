d57ef0fbbc9499660d1f6499403ba185a33b0c3f
homer2020 <ericmbaird@gmail.com>
2022-12-10 00:20:13 -0500

    init

--- Chap_12_Excel/readCensusExcel.py
+++ Chap_12_Excel/readCensusExcel.py
@@ -8,3 +8,11 @@ sheet = wb['Population by Census Tract']
 countyData = {}
 
 #TODO: Fill in countyData with each county's pop and tracts
+print('Reading rows...')
+for row in range(2, sheet.max_row + 1):
+    # Each row in the spreadsheet has data for one sensus tract
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
+#TODO: Open a new text file and write the contents of countyData to it
