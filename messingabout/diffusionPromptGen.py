244e8981360d670310c1b4181b90cab91cd52a2f
homer2020 <ericmbaird@gmail.com>
2022-12-12 18:51:11 -0500

    init

--- messingabout/diffusionPromptGen.py
+++ messingabout/diffusionPromptGen.py
@@ -7,10 +7,9 @@ import pyperclip
 print('Insert neutral prompts, enter between each prompt, enter nothing for next weighting:')
 #ask user for 1 weighting and so on.
 weighting = 0
-print('Enter prompt for ' + weighting + ' weighting:')
+print('Enter prompt for ' + str(weighting) + ' weighting:')
 #ask user for negative prompt
 print('Enter neutral negative prompt:')
 #ask user for weighting
-print('Negative prompt weighting ' + weighting)
+print('Negative prompt weighting ' + str(weighting)
 #spit out prompt to be copy and pasted to clipboard
-pyperclip.paste
