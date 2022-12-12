2437a86f4944309e839fd350a4fdc13de5dbb751
homer2020 <ericmbaird@gmail.com>
2022-12-12 18:49:52 -0500

    init

--- messingabout/diffusionPromptGen.py
+++ messingabout/diffusionPromptGen.py
@@ -1,5 +1,16 @@
 #! python3
 #  diffusionPromptGen.py - Adds weighting for stable difusion prompts and adds comma
 
+import pyperclip
+
 #ask user for neutral words
+print('Insert neutral prompts, enter between each prompt, enter nothing for next weighting:')
 #ask user for 1 weighting and so on.
+weighting = 0
+print('Enter prompt for ' + weighting + ' weighting:')
+#ask user for negative prompt
+print('Enter neutral negative prompt:')
+#ask user for weighting
+print('Negative prompt weighting ' + weighting)
+#spit out prompt to be copy and pasted to clipboard
+pyperclip.paste
