03ee128b401f7f25348c7f47928b80b8174526a2
homer2020 <ericmbaird@gmail.com>
2022-12-13 12:26:55 -0500

    init

---
+++ promptGen/promptGen.py
@@ -0,0 +1,8 @@
+#! python3
+# promptGen.py - Helps generate prompts for stable diffusion
+
+
+weighting = 0
+positivePrompt = []
+negativePrompt = []
+pPrompt = True  #Varible to toggle between positive and negative prompt
