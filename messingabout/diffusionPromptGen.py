763b16aab61d24ca892399bd914b8afb05f0512f
homer2020 <ericmbaird@gmail.com>
2022-12-12 20:59:49 -0500

    condensed functions

--- messingabout/diffusionPromptGen.py
+++ messingabout/diffusionPromptGen.py
@@ -6,9 +6,12 @@ import pyperclip
 def userPrompt():
     while True:
         prompt = str(input())
+        # if nothing entered ask if weight is wanted.
         if prompt == '':
+            addWeight()
             break
         else:
+            #if positie prompt add to positive list, if not negative list
             if pPrompt == True:
                 positivePrompt.append(('(' * weighting) + prompt + (')' * weighting))
             else:
@@ -18,9 +21,12 @@ def addWeight():
     while True:
         print('Would you like to add any weighted words? (y/n)')
         add = str(input())
+        #ask for added weight then prompt for word
         if add == 'y':
             global weighting
             weighting += 1
+            print('Prompt for weighting ' + str(weighting) + ':')
+            userPrompt()
             break
         elif add == 'n':
             break
@@ -35,15 +41,12 @@ pPrompt = True
 #ask user for neutral words
 print('Insert neutral prompts, enter between each prompt, enter nothing for next weighting:')
 userPrompt()
-#ask user for 1 weighting and so on.
-addWeight()
 
-
-print('Enter prompt for ' + str(weighting) + ' weighting:')
-userPrompt()
 #ask user for negative prompt
 print('Enter neutral negative prompt:')
 pPrompt = False
+weighting = 0
+userPrompt()
 
 #ask user for weighting
 print('Negative prompt for weighting ' + str(weighting) + ':')
