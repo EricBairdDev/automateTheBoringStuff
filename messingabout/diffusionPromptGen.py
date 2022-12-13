b9d874973a5c4bd1b5ed4a762a8e6af008bf792a
homer2020 <ericmbaird@gmail.com>
2022-12-12 20:05:38 -0500

    user prompt function

--- messingabout/diffusionPromptGen.py
+++ messingabout/diffusionPromptGen.py
@@ -3,26 +3,32 @@
 
 import pyperclip
 
+def userPrompt():
+    while True:
+        prompt = str(input())
+        if prompt == '':
+            break
+        else:
+            if pPrompt == True:
+                positivePrompt.append(prompt)
+            else:
+                negativePrompt.append(prompt)
+
+
 weighting = 0
 positivePrompt = []
 negativePrompt = []
-
+pPrompt = True
 #ask user for neutral words
-
-while True:
-    print('Insert neutral prompts, enter between each prompt, enter nothing for next weighting:')
-    prompt = str(input())
-    if prompt == '':
-        break
-    else:
-        positivePrompt.append(prompt)
-
+print('Insert neutral prompts, enter between each prompt, enter nothing for next weighting:')
+userPrompt()
 #ask user for 1 weighting and so on.
-while True:
-    print('Enter prompt for ' + str(weighting) + ' weighting:')
-    prompt = str(input())
+print('Enter prompt for ' + str(weighting) + ' weighting:')
+userPrompt()
 #ask user for negative prompt
 print('Enter neutral negative prompt:')
+pPrompt = False
+
 #ask user for weighting
 print('Negative prompt for weighting ' + str(weighting) + ':')
 #spit out prompt to be copy and pasted to clipboard
