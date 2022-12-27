0c894a870f79dbf942d03f1f541e11322e52052a
homer2020 <ericmbaird@gmail.com>
2022-12-27 13:11:48 -0500

    added bold to row/column A/1

--- Chap_12_Excel/multiplicationTable.py
+++ Chap_12_Excel/multiplicationTable.py
@@ -6,20 +6,20 @@ def create_multiplication_table(n):
     worksheet = workbook.active
 
     # Add the headers for the table
-    for i in range(1, n+1):
-        cell = worksheet.cell(row=1, column=i+1)
+    for i in range(1, n + 1):
+        cell = worksheet.cell(row=1, column= i + 1)
         cell.value = i
         cell.font = cell.font.copy(bold=True)
 
-    for i in range(1, n+1):
-        cell = worksheet.cell(row=i+1, column=1)
+    for i in range(1, n + 1):
+        cell = worksheet.cell(row = i + 1, column=1)
         cell.value = i
         cell.font = cell.font.copy(bold=True)
 
     # Fill in the rest of the table with the products of each row and column
-    for row in range(2, n+2):
-        for col in range(2, n+2):
-            worksheet.cell(row=row, column=col).value = (row-1) * (col-1)
+    for row in range(2, n + 2):
+        for col in range(2, n + 2):
+            worksheet.cell(row=row, column=col).value = (row - 1) * (col - 1)
 
     # Save the workbook
     workbook.save("multiplication_table.xlsx")
